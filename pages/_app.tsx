855f75526626386c5330bdbaeff3966c7ca3f903
hod25 <hod7112000@gmail.com>
2025-06-04 11:14:09 +0300

    he defult

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import { IntlProvider } from 'next-intl';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const locale = pageProps.locale || 'he';
   return (
-    <IntlProvider messages={pageProps.messages} locale={pageProps.locale}>
-      <Component {...pageProps} />
+    <IntlProvider messages={pageProps.messages} locale={locale}>
+      <Component {...pageProps} locale={locale} />
     </IntlProvider>
   );
 }
